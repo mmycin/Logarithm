6fca3852facc50e40be0c41b68837b5b3cae9cfc
Tahcin Ul Karim Mycin <mycin.mit@gmail.com>
2026-05-06 21:01:26 +0600

    feat: initialize application structure with main entry point and App component layout

--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,6 @@
 use crate::components::{FileBar, FileViewer, FilterTab, TitleBar};
 use leptos::prelude::*;
-use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"])]
-    async fn invoke(cmd: &str, args: JsValue) -> JsValue;
-}
 
 #[component]
 pub fn App() -> impl IntoView {
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,13 @@ pub mod components;
 
 use app::*;
 use leptos::prelude::*;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"])]
+    async fn invoke(cmd: &str, args: JsValue) -> JsValue;
+}
 
 fn main() {
     console_error_panic_hook::set_once();
